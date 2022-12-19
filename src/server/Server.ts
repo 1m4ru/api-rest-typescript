02088c8a542efd645d8b1ece4a1b612822a42f18
Maru <maruanibrahim22@gmail.com>
2022-12-18 21:27:53 -0300

    feat: init end points

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
+import {router} from './routes';
+
 const server = express();
 
-server.get("/", (req, res) => {
-    return res.send("ola, DEV!");
-});
+server.use(router);
+
 
 export { server };
